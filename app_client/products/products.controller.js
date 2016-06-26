e9ce522d052e876c0a6fb81b11cfe3a13cd17179
romenlaw <romen@pis.com.au>
2016-06-26 20:43:58 +1000

    fixes bug on rating calculation.

--- app_client/products/products.controller.js
+++ app_client/products/products.controller.js
@@ -196,83 +196,6 @@
                 }
             }
         };
-        //////////////////////////////////////////////////////////////////////////////////
-        /*
-        $scope.products = [
-            {
-                _id: "989803282",
-                name: "ABC Co.",
-                abn: '03284727229',
-                status: 'Active',
-                description: 'Large retailer of office supplies in the St George area, Sydney.',
-                tags: ['office', 'good'],
-                addresses: [
-                    {
-                        line1: '238 Bourke St',
-                        line2: '',
-                        suburb: 'Oatley',
-                        state: 'NSW',
-                        postcode: '2223',
-                        country: 'Australia'
-                    }
-                ],
-                email: 'admin@abc.com.au',
-                contacts: [
-                    {
-                        fullName: 'John Doe',
-                        gender: 'M',
-                        phoneNumber: '0298730984',
-                        mobileNumber: '0453987234',
-                        email: 'jdoe@abc.com.au',
-                        remarks: 'sales director'
-                    }
-                ]
-            },
-            {
-                _id: "098239873209",
-                name: "Babys R Us",
-                abn: '928282394333',
-                status: 'Active',
-                description: "product for infant and parenting needs",
-                tags: ['baby', 'parent'],
-                addresses: [
-                    {
-                        label: "head office",
-                        line1: "1 Big Ave",
-                        suburb: 'Melbourne',
-                        state: 'VIC',
-                        postcode: '3000',
-                    },
-                    {
-                        label: "warehouse",
-                        line1: "2 Sussex St",
-                        suburb: 'Sydney',
-                        state: 'NSW',
-                        postcode: '2000',
-                    }
-                ],
-                email: 'sales@babysrus.com',
-                contacts: [
-                    {
-                        fullName: 'Jane Doe',
-                        gender: 'F',
-                        phoneNumber: '0398757476',
-                        mobileNumber: '0454567234',
-                        email: 'jdoe@babiesrus.com',
-                        remarks: 'partner mgmt dpt.'
-                    },
-                    {
-                        fullName: "Chris O'Donnel",
-                        gender: 'M',
-                        phoneNumber: '0398757476',
-                        mobileNumber: '0454549873',
-                        email: 'codonnel@babiesrus.com',
-                        remarks: 'billing department'
-                    }
-                ]
-            }
-        ];
-         */
         
     };
     var setPristine = function (form) {
@@ -300,7 +223,7 @@
     }
     var calculateRating = function (product) {
         if (product.reviews.length > 0) {
-            product.reviews.map((c, i, arr)=> c.rating / arr.length).reduce((p, c) => c + p);
+            product.rating=product.reviews.map((c, i, arr)=> c.rating / arr.length).reduce((p, c) => c + p);
         }
     }
 })();
